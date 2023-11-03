957fee3cbd2d65e3cb3748749125604eb43bb618
seongeon0620 <believeon79@gmail.com>
2023-11-03 10:07:58 +0900

    first commit

---
+++ FCC/src/main/java/com/woori/myapp/FccApplication.java
@@ -0,0 +1,13 @@
+package com.woori.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FccApplication.class, args);
+	}
+
+}
