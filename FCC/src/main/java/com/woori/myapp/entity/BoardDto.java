62c4701647ffd82ba7e258a412ffccd2ae6662c1
Hayoung Yu <nhy8001@gmail.com>
2023-11-08 14:29:45 +0900

    [feat] 구현 board_write
    - board_write구현
    + 이미지 업로드 구현해야함

--- FCC/src/main/java/com/woori/myapp/entity/BoardDto.java
+++ FCC/src/main/java/com/woori/myapp/entity/BoardDto.java
@@ -4,66 +4,55 @@ import com.woori.myapp.common.BaseDto;
 
 public class BoardDto extends BaseDto{
 	private Long board_seq;
-    private String board_category;
-    private String board_title;
-    private String board_contents;
-    private String board_ingredients;
-    private	String board_wdate;
-    private Long member_seq;
+    private String board_category="";
+    private String board_title="";
+    private String board_contents="";
+    private String board_ingredients="";
+    private	String board_wdate="";
+    private Long member_seq=1L;
+	public Long getBoard_seq() {
+		return board_seq;
+	}
+	public void setBoard_seq(Long board_seq) {
+		this.board_seq = board_seq;
+	}
+	public String getBoard_category() {
+		return board_category;
+	}
+	public void setBoard_category(String board_category) {
+		this.board_category = board_category;
+	}
+	public String getBoard_title() {
+		return board_title;
+	}
+	public void setBoard_title(String board_title) {
+		this.board_title = board_title;
+	}
+	public String getBoard_contents() {
+		return board_contents;
+	}
+	public void setBoard_contents(String board_contents) {
+		this.board_contents = board_contents;
+	}
+	public String getBoard_ingredients() {
+		return board_ingredients;
+	}
+	public void setBoard_ingredients(String board_ingredients) {
+		this.board_ingredients = board_ingredients;
+	}
+	public String getBoard_wdate() {
+		return board_wdate;
+	}
+	public void setBoard_wdate(String board_wdate) {
+		this.board_wdate = board_wdate;
+	}
+	public Long getMember_seq() {
+		return member_seq;
+	}
+	public void setMember_seq(Long member_seq) {
+		this.member_seq = member_seq;
+	}
+    
+    
 
-    public Long getBoardSeq() {
-        return board_seq;
-    }
-
-    public void setBoardSeq(Long boardSeq) {
-        this.board_seq = boardSeq;
-    }
-
-    public String getBoardCategory() {
-        return board_category;
-    }
-
-    public void setBoardCategory(String boardCategory) {
-        this.board_category = boardCategory;
-    }
-
-    public String getBoardTitle() {
-        return board_title;
-    }
-
-    public void setBoardTitle(String boardTitle) {
-        this.board_title = boardTitle;
-    }
-
-    public String getBoardContents() {
-        return board_contents;
-    }
-
-    public void setBoardContents(String boardContents) {
-        this.board_contents = boardContents;
-    }
-
-    public String getBoardIngredients() {
-        return board_ingredients;
-    }
-
-    public void setBoardIngredients(String boardIngredients) {
-        this.board_ingredients = boardIngredients;
-    }
-
-    public String getBoardWDate() {
-        return board_wdate;
-    }
-
-    public void setBoardWDate(String boardWDate) {
-        this.board_wdate = boardWDate;
-    }
-
-    public Long getMemberSeq() {
-        return member_seq;
-    }
-
-    public void setMemberSeq(Long memberSeq) {
-        this.member_seq = memberSeq;
-    }
 }
