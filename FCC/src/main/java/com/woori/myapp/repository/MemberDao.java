5fec1f8c79dbbcb4e1ceb3b04f75bed8985f02eb
Hyunbo <hbo9347@naver.com>
2023-11-08 23:41:16 +0900

    [Feat] 회원가입 유효성검사, nav.html - mypage 이동구현

--- FCC/src/main/java/com/woori/myapp/repository/MemberDao.java
+++ FCC/src/main/java/com/woori/myapp/repository/MemberDao.java
@@ -1,14 +1,17 @@
 package com.woori.myapp.repository;
 
-
 import com.woori.myapp.entity.MemberDto;
 
 public interface MemberDao {
 
 	void insert(MemberDto dto);
+
 	MemberDto login_proc(MemberDto dto);
+
 	MemberDto getMypage(MemberDto dto);
+
 	boolean idCheck(MemberDto dto);
+
 	void update(MemberDto dto);
 
 }
