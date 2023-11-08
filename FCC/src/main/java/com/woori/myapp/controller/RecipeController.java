718cf25f4dbf37d26ee4dfab1da2ab30bd24f2e0
jjaemm24 <148323426+jjaemm24@users.noreply.github.com>
2023-11-09 01:07:06 +0900

    [Feat] 찜 기능 구현
    LikesDto 추가
    LikesDto관련 mybatis-config.xml 수정
    찜기능 관련 파일 수정

--- FCC/src/main/java/com/woori/myapp/controller/RecipeController.java
+++ FCC/src/main/java/com/woori/myapp/controller/RecipeController.java
@@ -1,15 +1,20 @@
 package com.woori.myapp.controller;
 
+
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.woori.myapp.common.Pager;
 import com.woori.myapp.entity.RecipeDto;
 import com.woori.myapp.service.RecipeService;
 
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class RecipeController {
@@ -21,7 +26,7 @@ public class RecipeController {
 	public String recipe_list(Model model, RecipeDto dto) {
 		
 		model.addAttribute("recipeList", service.getRecipeList(dto));	
-		model.addAttribute("page", Pager.makePage(9, service.getRecipeTotalCnt(), 0));	
+		model.addAttribute("page", Pager.makePage(12, service.getRecipeTotalCnt(), 0));	
 		return "/recipe/recipe_list";
 	}
 	
@@ -35,11 +40,27 @@ public class RecipeController {
 	}
 	
 	@GetMapping("/recipe/view/{rcp_seq}")
-	public String recipe_view(Model model, @PathVariable("rcp_seq") Integer rcp_seq) {
+	public String recipe_view(HttpServletRequest request, Model model, @PathVariable("rcp_seq") Integer rcp_seq) {
 		RecipeDto recipeInfo = service.getRecipeInfo(rcp_seq);
 		model.addAttribute("recipeInfo", recipeInfo);
 		model.addAttribute("recipeAnalysis", service.analyzeRecipe(recipeInfo));
+		model.addAttribute("checkRecipeLike", service.checkRecipeLike(request, rcp_seq));
 		return "/recipe/recipe_view";
+	}
+	
+	@GetMapping("/recipe/like/{rcp_seq}")
+	@ResponseBody
+	public HashMap<String, Object> recipe_like(HttpServletRequest request, String like_type, @PathVariable("rcp_seq") Integer rcp_seq) {
+
+		HashMap<String, Object> map = service.likeRecipe(request, like_type, rcp_seq);
+		return map;
+	}
+	
+	@GetMapping("/recipe/like/list")
+	public String recipe_like_list(Model model, HttpServletRequest request) {
+		
+		model.addAttribute("recipeList", service.getRecipeLikeList(request));
+		return "/recipe/recipe_like_list";
 	}
 	
 }
